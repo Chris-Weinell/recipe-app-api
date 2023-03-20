f73e4e7991ae6ed9a83164c23c46d6d50436fff9
Chris Weinell <weinellcj@live.com>
2023-03-20 14:56:30 -0400

    implement recipe ingredient functionality

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -31,20 +31,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(
-    mixins.DestroyModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet
-):
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     #   Note - If I import viewsets.ModelViewSet as I did for the RecipeViewSet instead
     #          of the mixins, the tests still pass successfully.
-    """Manage Tags in the database."""
-    serializer_class = serializers.TagSerializer
-    queryset = Tag.objects.all()
+    """Base Viewset for Recipe Attributes"""
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """Retrieve Tags for authenticated user."""
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage Tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage Ingredients in the db."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
